080d66ce45c456d700f60ae16a07c97b2d24e704
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-05-20 11:54:26 -0400

    commit tree d3e40d1d9805fd36c04304c7c796214d9ad0b996

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+function saturdayFun(activity = `roller-skate`) {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = `go to the office`) {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = `*`) {
+    return function(something = `something`) {
+        return `You are ${flair}${something}${flair}!`
+    }
+}
+
+const Calculator = {
+    add: function(x, y) {return x + y},
+    subtract: function(x, y) {return x - y},
+    multiply: function(x, y) {return x * y},
+    divide: function(x, y) {return x / y}
+}
+
+
+function actionApplyer(int, arr) {
+    let x = int
+    for (let instance of arr){
+        x = instance(x)
+
+    }
+    return x
+}
